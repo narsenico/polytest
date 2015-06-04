4a24714765ac4ae324eeb55fa7c5070ce17d0705
narsenico <n3r0n3@78>
2015-06-04 17:07:04 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,134 @@
+'use strict';
+
+//see https://github.com/PolymerElements/polymer-starter-kit/blob/master/gulpfile.js
+
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var path = require('path');
+var fs = require('fs');
+var del = require('del');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var merge = require('merge-stream');
+
+var AUTOPREFIXER_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
+
+var styleTask = function (stylesPath, srcs) {
+  return gulp.src(srcs.map(function(src) {
+      return path.join('app', stylesPath, src);
+    }))
+    .pipe($.changed(stylesPath, {extension: '.css'}))
+    .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
+    .pipe(gulp.dest('.tmp/' + stylesPath))
+    .pipe($.if('*.css', $.cssmin()))
+    .pipe(gulp.dest('dist/' + stylesPath))
+    .pipe($.size({title: stylesPath}));
+};
+
+// Compile and Automatically Prefix Stylesheets
+gulp.task('styles', function () {
+  return styleTask('styles', ['**/*.css']);
+});
+
+gulp.task('elements', function () {
+  return styleTask('elements', ['**/*.css']);
+});
+
+// Optimize Images
+gulp.task('images', function () {
+  return gulp.src('app/images/**/*')
+    // .pipe($.cache($.imagemin({
+    //   progressive: true,
+    //   interlaced: true
+    // })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe($.size({title: 'images'}));
+});
+
+// Copy All Files At The Root Level (app)
+gulp.task('copy', function() {
+    var app = gulp.src([
+        'app/*'
+    ], {
+        dot: true
+    }).pipe(gulp.dest('dist'));
+
+    var bower = gulp.src([
+        'bower_components/**/*'
+    ]).pipe(gulp.dest('dist/bower_components'));
+
+    var elements = gulp.src(['app/elements/**/*.html'])
+        .pipe(gulp.dest('dist/elements'));
+
+    var swBootstrap = gulp.src(['bower_components/platinum-sw/bootstrap/*.js'])
+        .pipe(gulp.dest('dist/elements/bootstrap'));
+
+    var swToolbox = gulp.src(['bower_components/sw-toolbox/*.js'])
+        .pipe(gulp.dest('dist/sw-toolbox'));
+
+    var vulcanized = gulp.src(['app/elements/elements.html'])
+        .pipe($.rename('elements.vulcanized.html'))
+        .pipe(gulp.dest('dist/elements'));
+
+    return merge(app, bower, elements, vulcanized, swBootstrap, swToolbox)
+        .pipe($.size({
+            title: 'copy'
+        }));
+});
+
+// Clean Output Directory
+gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+
+// Watch Files For Changes & Reload
+gulp.task('serve', ['styles', 'elements', 'images'], function () {
+  browserSync({
+    notify: false,
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: {
+      baseDir: ['.tmp', 'app'],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+
+  gulp.watch(['app/**/*.html'], reload);
+  gulp.watch(['app/styles/**/*.css'], ['styles', reload]);
+  gulp.watch(['app/elements/**/*.css'], ['elements', reload]);
+  gulp.watch(['app/{scripts,elements}/**/*.js'], ['jshint']);
+  gulp.watch(['app/images/**/*'], reload);
+});
+
+// Build and serve the output from the dist build
+gulp.task('serve:dist', ['default'], function () {
+  browserSync({
+    notify: false,
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: 'dist'
+  });
+});
+
+// Default task
+gulp.task('default', ['clean'], function(cb) {
+    runSequence(
+    	['copy', 'styles'],
+    	'elements',
+        cb);
+});
